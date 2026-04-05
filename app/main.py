c0f925c250125e5767bed5f00e2ce3519efb8681
haggaikar-maker <haggaikar@gmail.com>
2026-04-06 00:44:10 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-
+from app.routers import preferences
 from app.config import settings
 from app.routers import auth, pages
 
 app = FastAPI(title=settings.APP_NAME)
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
-
+app.include_router(preferences.router)
 app.include_router(auth.router)
 app.include_router(pages.router)
