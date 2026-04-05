2d11314e98ff902963bee9e941119b3c1b689c32
haggaikar-maker <haggaikar@gmail.com>
2026-04-06 00:43:07 +0300

    Update __init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,6 @@
 from app.models.user import User
+from app.models.schedule_week import ScheduleWeek
+from app.models.shift import Shift
+from app.models.preference import Preference
 
 __all__ = ["User"]
